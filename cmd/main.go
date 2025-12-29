c766a212c12a7af4d7dfbd73fe661fcd8cdf0fe6
Anshualawa <alawapappu@gmail.com>
2025-12-29 20:36:49 +0530

    load env file

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"pastebin-lite/handlers"
 	"pastebin-lite/store"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	fmt.Println("RedisURL:", os.Getenv("REDIS_URL"))
 	if err := store.InitRedis(); err != nil {
 		log.Fatalf("redis connection failed: %v", err)
 	}
